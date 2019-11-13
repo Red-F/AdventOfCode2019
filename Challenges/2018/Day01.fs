7201f026c7c8b974a3ba6135a82eedbc4393ebc6
Fred Hoogduin <dev@hoogduin.net>
2019-11-13 09:10:58 +0100

    day 1 solved

---
+++ Challenges/2018/Day01.fs
@@ -0,0 +1,19 @@
+﻿module Year2018Day01
+
+open AdventOfCode.Common
+
+let repeatSequence deltas =
+    seq { while true do yield! deltas }
+
+let runningTotal data = 
+  Seq.scan (+) 0 data |> Seq.skip 1
+
+let filterDuplicates (data : seq<int>) =
+    let h = System.Collections.Generic.HashSet()
+    seq { for i in data do match h.Add i with | true -> () | false -> yield i }
+
+let solvePart2 deltas =
+    repeatSequence deltas |> runningTotal |> filterDuplicates |> Seq.head
+
+let solver =
+    { parse = parseEachLine asInt; part1 = Seq.sum; part2 = solvePart2 }
