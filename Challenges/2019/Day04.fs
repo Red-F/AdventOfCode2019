2ad541d4bb18915b175c255116e4b7c8b03ad002
Fred Hoogduin <f.hoogduin@tjip.com>
2019-12-04 09:18:51 +0100

    day 4 of 2019

--- Challenges/2019/Day04.fs
+++ Challenges/2019/Day04.fs
@@ -1,13 +1,27 @@
 module Year2019Day04
 
+open System
 open AdventOfCode.Common
 
-let solvePart1 modules  =
-  0
+let isIncreasing (s : string) =
+  let ctoi c = int c - int '0'
+  s |> Seq.toList |> Seq.fold (fun (d, f) elem -> (ctoi elem, f && ctoi elem >= d)) (-1, true) |> snd
 
-let solvePart2 modules =
-  0
-  
+let doubled (c : char) =
+  String.Concat(c, c)
+
+let hasAdjacentDigits (s : string) =
+  s |> Seq.toList |> Seq.fold (fun acc elem ->  acc || s.IndexOf(doubled elem) >= 0) false
+
+let hasExactlyDoubleDigits (s : string) =
+  let exactlyDoubleChar (str : string) c = str.IndexOf(doubled c) >= 0 && str.IndexOf(doubled c) = str.LastIndexOf(doubled c)
+  s |> Seq.toList |> Seq.fold (fun acc elem -> acc || exactlyDoubleChar s elem) false
+
+let solvePart1 (range : int array)  =
+  seq {for i in range.[0]..range.[1] -> string i} |> Seq.filter(fun s -> isIncreasing s && hasAdjacentDigits s) |> Seq.length
+
+let solvePart2 (range : int array) =
+  seq {for i in range.[0]..range.[1] -> string i} |> Seq.filter(fun s -> isIncreasing s && hasExactlyDoubleDigits s) |> Seq.length
+
 let solver =
-    { parse = parseEachLine asInt; part1 = solvePart1; part2 = solvePart2 }
- 
+    { parse = parseFirstLine (splitBy "-" asIntArray); part1 = solvePart1; part2 = solvePart2 }
