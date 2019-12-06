db7b9f3350426886f45d21dd4c510d3fae043ec5
Fred Hoogduin <dev@hoogduin.net>
2019-12-06 11:59:19 +0100

    day 6 of 2019

--- Challenges/2019/Day06.fs
+++ Challenges/2019/Day06.fs
@@ -2,12 +2,41 @@ module Year2019Day06
 
 open AdventOfCode.Common
 
-let solvePart1 modules  =
-  0
+type Planet = { name : string; indirect : int; planets : Planet list }
 
-let solvePart2 modules =
-  0
+let asOrbit = splitBy ")" (fun x -> (x.[0], x.[1]))
+
+let rec toConstellation name (depth : int) orbits =
+  let inOrbit = orbits |> List.filter(fun (a,_) -> a = name)
+  let others = orbits |> List.filter(fun (a,_) -> a <> name)
+  match inOrbit with
+  | [] -> {name=name; indirect=depth; planets=[]}
+  | _ ->  {name=name; indirect=depth; planets= inOrbit |> List.map(fun (_,b) -> toConstellation b (depth+1) others)}
+
+let rec traverseConstellation (p : Planet) =
+  match p with
+  | {planets=[]}  -> 1 + p.indirect
+  | _ ->  p.planets |> List.fold(fun acc elem -> acc + traverseConstellation elem) (p.indirect + 1)
+
+let rec findName s (l: Planet list) (p : Planet) =
+  match p with
+  | _ when p.name = s -> (true, p :: l |> List.fold(fun acc elem -> elem :: acc ) [])
+  | _ when List.isEmpty p.planets -> (false, [])
+  | _ -> p.planets |> List.fold(fun (found, planets) elem -> match found with | true -> (found, planets) | false -> findName s (p :: l) elem) (false, l)
+
+let rec orbitalTransfers (l1 : Planet list) (l2 : Planet list) =
+  match l1, l2 with
+  | h1::t1, h2::t2 when h1 = h2 -> orbitalTransfers t1 t2
+  | _::t1, _::t2 -> t1.Length + t2.Length
+  | _ -> invalidArg "l1, l2" "do not pas empty list here"
+
+let solvePart1 orbits   =
+  toConstellation "COM" -1 (orbits |> Seq.toList) |> traverseConstellation
+
+let solvePart2 orbits =
+  let constellation = toConstellation "COM" -1 (orbits |> Seq.toList)
+  orbitalTransfers (constellation |> findName "YOU" [] |> snd) (constellation |> findName "SAN" [] |> snd)
   
 let solver =
-    { parse = parseEachLine asInt; part1 = solvePart1; part2 = solvePart2 }
+    { parse = parseEachLine asOrbit; part1 = solvePart1; part2 = solvePart2 }
  
